6fc6896d4bc46c00af5fe1462e7a15036280ce47
sahib bajaj <bajajsaib3@gmail.com>
2021-03-22 20:05:47 +0530

    d

--- src/components/App.js
+++ src/components/App.js
@@ -5,34 +5,38 @@ class Timer extends React.Component {
     super(props);
     this.state = { time: 0, x: 0, y: 0, started: false };
     this.timerInterval = null;
+    this.keyListener = this.keyListener.bind(this);
+    this.gameStart = this.gameStart.bind(this);
   }
-  KeyListener = (evt) => {
+
+    keyListener(evt)  {
+    console.log(evt);
     if (this.state.started) {
-      if (evt.KeyCode === 37) {
+      if (evt.keyCode === 37) {
         this.setState({ x: this.state.x - 5 })
       }
-      else if (evt.KeyCode === 38) {
+      else if (evt.keyCode === 38) {
         this.setState({ y: this.state.y - 5 })
       }
-      else if (evt.KeyCode === 39) {
+      else if (evt.keyCode === 39) {
         this.setState({ x: this.state.x + 5 })
       }
-      else if (evt.KeyCode === 40) {
+      else if (evt.keyCode === 40) {
         this.setState({ y: this.state.y + 5 })
       }
 
     }
   }
-  gameStart = () => {
-    this.setState({
-      started: true,
-    });
-    this.timerInterval = setInterval(() =>
-      this.setState({ timer: this.state.time + 1 }), 1 * 1000);
-    document.addEventListener("keydown", this.KeyListener);
+  gameStart()  {    
+    console.log ( "...sahib...");
+    this.setState({  started: true });
+   this.timerInterval = setInterval(() =>
+      this.setState({ time: this.state.time + 1 }), 1 * 1000);
+    
+      document.addEventListener("keydown", this.keyListener);
   }
   componentDidMount() {
-
+    //document.addEventListener("keydown", this.keyListener);
   }
   componentDidUpdate() {
     if (this.state.x === 250 && this.state.y === 250) {
@@ -41,7 +45,7 @@ class Timer extends React.Component {
   }
 
   componentWillUnmount() {
-
+    document.addEventListener("keydown", this.keyListener);
   }
 
 
@@ -49,7 +53,7 @@ class Timer extends React.Component {
   render() {
     return (
       <>
-        {!this.state.started ? <button className='start' onClick={this.gameStart}></button> :
+        {!this.state.started ? <button className='start' onClick={this.gameStart}> click me</button> :
           (<>
             <div className="ball" style={{ left: this.state.x + "px", top: this.state.y + 'px' }}> </div>
             <div className="hole" style={{ left: "250px", top: '250px' }}></div>
